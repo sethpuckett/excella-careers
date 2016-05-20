b8cf87e571c43de741999f2f09af21a575ad57a7
Seth Puckett <puckett.seth@gmail.com>
2016-05-19 23:26:50 -0400

    added web load logic to splash screen

--- ExcellaCareers/ExcellaCareers.Droid/MainActivity.cs
+++ ExcellaCareers/ExcellaCareers.Droid/MainActivity.cs
@@ -1,35 +1,26 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using ExcellaCareers.Model;
 using ExcellaCareers.Services;
 using ExcellaCareers.Services.Impl;
+using Newtonsoft.Json;
 
 namespace ExcellaCareers.Droid
 {
     [Activity (Label = "Excella Careers", Icon = "@drawable/ExcellaLogo")]
     public class MainActivity : ListActivity
     {
-        private readonly IHtmlScraper htmlScraper;
-
-        private readonly ICareerHtmlParser careerHtmlParser;
-        
-        public MainActivity()
-        {
-            this.htmlScraper = new HtmlScraper();
-            this.careerHtmlParser = new CareerHtmlParser();
-        }
-
-        protected override async void OnCreate (Bundle bundle)
+        protected override void OnCreate (Bundle bundle)
         {
             base.OnCreate (bundle);
             SetContentView (Resource.Layout.Main);
-            
-            var webResponse = await this.htmlScraper.Scrape(Resources.GetString(Resource.String.careers_url));
 
-            var jobs = this.careerHtmlParser.ParseHtml(webResponse);
-            
+            var jobJson = Intent.GetStringExtra("jobs");
+            var jobs = JsonConvert.DeserializeObject<IEnumerable<Job>>(jobJson);
+
             this.ListAdapter = new JobListItemAdapter(this, Resource.Layout.CareerListItem, jobs.ToList());
 
             this.ListView.ItemClick += (sender, e) =>
--- ExcellaCareers/ExcellaCareers.Droid/SplashActivity.cs
+++ ExcellaCareers/ExcellaCareers.Droid/SplashActivity.cs
@@ -1,4 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
@@ -7,26 +11,51 @@ using Android.Support.V7.App;
 using ExcellaCareers.Model;
 using ExcellaCareers.Services;
 using ExcellaCareers.Services.Impl;
+using Newtonsoft.Json;
 
 namespace ExcellaCareers.Droid
 {
     [Activity(Label = "Excella Careers", MainLauncher = true, Icon = "@drawable/ExcellaLogo", Theme = "@style/SplashTheme", NoHistory = true)]
     public class SplashActivty : AppCompatActivity
     {
-        protected override void OnResume()
+        private readonly IHtmlScraper htmlScraper;
+
+        private readonly ICareerHtmlParser careerHtmlParser;
+
+        public SplashActivty()
+        {
+            this.htmlScraper = new HtmlScraper();
+            this.careerHtmlParser = new CareerHtmlParser();
+        }
+
+        protected override async void OnResume()
         {
             base.OnResume();
 
-            var startupWork = new Task(() => {
-                Task.Delay(3000);  // Simulate a bit of startup work.
-            });
-
-            startupWork.ContinueWith(t => {
-                StartActivity(new Intent(Application.Context, typeof(MainActivity)));
-            }, TaskScheduler.FromCurrentSynchronizationContext());
-
-            startupWork.Start();
+            var jobs = await this.LoadJobs();
+            this.LaunchMainActivity(jobs);
         }
 
+        private async Task<IEnumerable<Job>> LoadJobs()
+        {
+            try
+            {
+                var webResponse = await this.htmlScraper.Scrape(Resources.GetString(Resource.String.careers_url));
+                var jobs = this.careerHtmlParser.ParseHtml(webResponse);
+                return jobs;
+            }
+            catch (WebException e)
+            {
+                return null;
+            }
+        }
+
+        private void LaunchMainActivity(IEnumerable<Job> jobs)
+        {
+            var intent = new Intent(Application.Context, typeof(MainActivity));
+            intent.PutExtra("jobs", JsonConvert.SerializeObject(jobs));
+            StartActivity(intent);
+        }
+        
     }
 }
