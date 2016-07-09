4f713a6d3bee246127fee2e69d11bf4b720be6de
Seth Puckett <puckett.seth@gmail.com>
2016-07-09 18:57:30 -0400

    Updating main storyboard

--- ExcellaCareers/ExcellaCareers.iOS/ViewController.cs
+++ ExcellaCareers/ExcellaCareers.iOS/ViewController.cs
@@ -15,12 +15,6 @@ namespace ExcellaCareers.iOS
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
-			// Perform any additional setup after loading the view, typically from a nib.
-			Button.AccessibilityIdentifier = "myButton";
-			Button.TouchUpInside += delegate {
-				var title = string.Format ("{0} clicks!", count++);
-				Button.SetTitle (title, UIControlState.Normal);
-			};
 		}
 
 		public override void DidReceiveMemoryWarning ()
